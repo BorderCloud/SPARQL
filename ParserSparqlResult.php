e8fcf661af20ad1a48d8ec64e2af35011f9e4816
karima <karima.rafes@gmail.com>
2014-10-01 22:58:52 +0200

    Fix blanknode

--- ParserSparqlResult.php
+++ ParserSparqlResult.php
@@ -151,21 +151,20 @@ class ParserSparqlResult extends Base {
       $difference=array();
       //A/ Check the variables lists in the header are the same.
       if(! isset($rs1['result']['variables']) && ! isset($rs2['result']['variables'])){
-	  return $difference; //return true ;
+          return $difference; //return true ;
       }elseif (! isset($rs1['result']['variables']) || ! isset($rs2['result']['variables']) ) {
-	  $difference[1]=$rs1['result']['variables'];
-	  $difference[2]=$rs2['result']['variables'];
-	  return $difference; //return false ;
+          $difference[1]=$rs1['result']['variables'];
+          $difference[2]=$rs2['result']['variables'];
+          return $difference; //return false ;
       }
-      
+
       $difference=array_diff($rs1,$rs2);
       if (count($difference) != 0) {
-	  return $difference; //return false ;
+          return $difference; //return false ;
       }
-      
+
      //Check if there are blanknodes//////////////////////
       //ref : http://blog.datagraph.org/2010/03/rdf-isomorphism
-     echo "RRRRRRRRRRRRRRRRRRRRRRRRR\n";
       
 // 1.Compare graph sizes and all statements without blank nodes. If they do not match, fail.
 //1.1 remove blank nodes
@@ -173,131 +172,162 @@ class ParserSparqlResult extends Base {
      $clone2WithoutBlanknodes = $rs2['result']['rows'];
      $bnodesInRs1=array();
      $bnodesInRs2=array();
-      
+
      // echo "AVANT";
-	//  print_r($clone1);
-	//  print_r($clone2);
+        //  print_r($clone1);
+        //  print_r($clone2);
       foreach ($clone1WithoutBlanknodes as $key=>&$row) {
          $arrayVariableTypeBnode = array_keys( $row , "bnode") ;
-	 foreach ($arrayVariableTypeBnode as $variableTypeBnode) {
-	       $bnodesInRs1[] = $row[split(" ",$variableTypeBnode)[0]] ;
-	       $row[split(" ",$variableTypeBnode)[0]] = "BLANKNODE";//remove
-	  }
+         foreach ($arrayVariableTypeBnode as $variableTypeBnode) {
+                $variableArray = split(" ",$variableTypeBnode);
+                $variable=$variableArray[0];
+                $bnodesInRs1[] = $row[$variable] ;
+               $row[$variable] = "BLANKNODE";//remove
+          }
       }
       foreach ( $clone2WithoutBlanknodes as $key=>&$row) {
          $arrayVariableTypeBnode = array_keys( $row , "bnode") ;
-	 foreach ($arrayVariableTypeBnode as $variableTypeBnode) {
-	       $bnodesInRs2[] = $row[split(" ",$variableTypeBnode)[0]] ;
-	       $row[split(" ",$variableTypeBnode)[0]] = "BLANKNODE";//remove
-	  }
+         foreach ($arrayVariableTypeBnode as $variableTypeBnode) {
+                $variableArray = split(" ",$variableTypeBnode);
+                $variable=$variableArray[0];
+               $bnodesInRs2[] = $row[$variable] ;
+               $row[$variable] = "BLANKNODE";//remove
+          }
       }
-      
-	  print_r($clone1WithoutBlanknodes);
-	  print_r($clone2WithoutBlanknodes);
+
+          //print_r($clone1WithoutBlanknodes);
+          //print_r($clone2WithoutBlanknodes);
 //1.2 compare
       $difference =  self::sub_array_diff_assoc_unordered( $clone1WithoutBlanknodes, $clone2WithoutBlanknodes) ;
-      
-      
+
+
       if((count($bnodesInRs1) == 0 && count($bnodesInRs2) == 0 )  || count($difference) != 0)
-	  return $difference;
+          return $difference;
+
+        $bnodesInRs1=array_values(array_unique($bnodesInRs1));
+        $bnodesInRs2=array_values(array_unique($bnodesInRs2));
+        if(count($bnodesInRs1) != count($bnodesInRs2)) {
+            $difference[1]="Nb bnode :".count($bnodesInRs1);
+            $difference[2]="Nb bnode :".count($bnodesInRs2);
+            return $difference; //return false ;
+        }
 
         //echo "BLANKNODE\n";     
-	//  print_r($bnodesInRs1);
-	//  print_r($bnodesInRs2);
-     
+        //print_r($bnodesInRs1);
+        //print_r($bnodesInRs2);
+
       //$difference =  self::sub_array_diff_assoc_unordered( $rs1['result']['rows'], $rs2['result']['rows']) ;
-     
-     
+
       $clone1 = $rs1['result']['rows'];
-	    print_r($clone1);
+        //    print_r($clone1);
       $clone2 = $rs2['result']['rows'];
       // 2.Repeat, for each graph:
       $arrayPermutationsBnode = self::AllPermutations($bnodesInRs2);
-      foreach ( $arrayPermutationsBnode as $permute) {
-	  foreach ( $clone2 as $key=>&$row) {
-	    $arrayVariableTypeBnode = array_keys( $row , "bnode") ;
-	    foreach ($arrayVariableTypeBnode as $variableTypeBnode) {
-		  $variable = split(" ",$variableTypeBnode)[0];
-		  $row[$variable] = $bnodesInRs1[array_search($row[$variable] ,$permute)];
-	      }
-	  }
-	  
-	    print_r($clone2);
-	  $difference =  self::sub_array_diff_assoc_unordered( $clone1,$clone2) ;
-	  if(count($difference) == 0)
-		return $difference; //true
+        //echo "PERMUTATION\n";
+        //print_r($arrayPermutationsBnode );      
+        //exit();
+        foreach ( $arrayPermutationsBnode as $permute) {
+                //print_r($permute);
+
+          foreach ( $clone2 as $key=>&$row) {
+            $arrayVariableTypeBnode = array_keys( $row , "bnode") ;
+            foreach ($arrayVariableTypeBnode as $variableTypeBnode) {
+                  $variableArray = split(" ",$variableTypeBnode);
+                  $variable=$variableArray[0];
+
+                  $row[$variable] = $bnodesInRs1[array_search($row[$variable] ,$permute)];
+              }
+          }
+
+            //print_r($clone2);
+          $difference =  self::sub_array_diff_assoc_unordered( $clone1,$clone2) ;
+          if(count($difference) == 0){
+                return $difference; //true
+          }
       }
-      
+
       return $difference;
   }
-    
+
     private static function sub_array_diff_assoc_unordered( $rows1,  $rows2) {
-	$difference=array();
+        $difference=array();
 
-	//B/ Check the result set have the same number of rows.
-	if(count($rows1) != count($rows2)) {
-	    $difference[1]="Nb rows :".count($rows1);
-	    $difference[2]="Nb rows :".count($rows2);
-	    return $difference; //return false ;
-	}
+        //B/ Check the result set have the same number of rows.
+        if(count($rows1) != count($rows2)) {
+            $difference[1]="Nb rows :".count($rows1);
+            $difference[2]="Nb rows :".count($rows2);
+            return $difference; //return false ;
+        }
 
-	//C/ Pick a row from the test results, scan the expected results
-	//   to find a row with same variable/value bindings, and remove
-	//   from the expected results. If all test rows, match then
-	//   (because of B) the result sets have the same rows.
-	//   
-	//return equivalent(convert(rs1), convert(rs2), new BNodeIso(NodeUtils.sameValue)) ;
-	$clone1 = $rows1;
-	$clone2 = $rows2;
-	
+        //C/ Pick a row from the test results, scan the expected results
+        //   to find a row with same variable/value bindings, and remove
+        //   from the expected results. If all test rows, match then
+        //   (because of B) the result sets have the same rows.
+        //   
+        //return equivalent(convert(rs1), convert(rs2), new BNodeIso(NodeUtils.sameValue)) ;
+        $clone1 = $rows1;
+        $clone2 = $rows2;
+
         // echo "AVANT";
-	  //  print_r($clone1);
-	  //  print_r($clone2);
-	foreach ($rows1 as $key1=>&$value1) {
-	    $tmpclone2 = $clone2;
-	      foreach ($tmpclone2 as $key2=>&$value2) {
-		  
-		//echo "-------------";
-		//print_r($value1);
-		//print_r($value2);
-		if(count(array_diff_assoc($value1,$value2)) == 0 && 
-		    count(array_diff_assoc($value2,$value1)) == 0 ){
-		      unset($clone1[$key1]);
-		      unset($clone2[$key2]);
-		      break;
-		}
-	      }
-	      //echo "-------------APRES";
-	      //print_r($clone1);
-	      //print_r($clone2);
-	}
+          //  print_r($clone1);
+          //  print_r($clone2);
+        foreach ($rows1 as $key1=>&$value1) {
+            $tmpclone2 = $clone2;
+              foreach ($tmpclone2 as $key2=>&$value2) {
 
-	if(count($clone1) != 0 || 
-	    count($clone2) != 0 ){
-	    $difference[1]=$clone1;
-	    $difference[2]=$clone2;
-	    return $difference; //return false ;
-	}
-	return $difference;
+                //echo "-------------";
+                //print_r($value1);
+                //print_r($value2);
+                if(count(array_diff_assoc($value1,$value2)) == 0 &&
+                    count(array_diff_assoc($value2,$value1)) == 0 ){
+                      unset($clone1[$key1]);
+                      unset($clone2[$key2]);
+                      break;
+                }
+              }
+              //echo "-------------APRES";
+              //print_r($clone1);
+              //print_r($clone2);
+        }
+
+        if(count($clone1) != 0 ||
+            count($clone2) != 0 ){
+            $difference[1]=$clone1;
+            $difference[2]=$clone2;
+            $difference[1]=$clone1;
+            $difference[2]=$clone2;
+            return $difference; //return false ;
+        }
+        return $difference;
       }
-      
-      private static function AllPermutations($InArray, $InProcessedArray = array())
-      {
-	  $ReturnArray = array();
-	  foreach($InArray as $Key=>$value)
-	  {
-	      $CopyArray = $InProcessedArray;
-	      $CopyArray[$Key] = $value;
-	      $TempArray = array_diff_key($InArray, $CopyArray);
-	      if (count($TempArray) == 0)
-	      {
-		  $ReturnArray[] = $CopyArray;
-	      }
-	      else
-	      {
-		  $ReturnArray = array_merge($ReturnArray, AllPermutations($TempArray, $CopyArray));
-	      }
-	  }
-	  return $ReturnArray;
-      }
+
+      private static function AllPermutations($set)
+        {
+                $solutions=array();
+                $n=count($set);
+                $p=array_keys($set);
+                $i=1;
+
+                while ($i<$n)
+                {
+                  if ($p[$i]>0){
+                        $p[$i]--;
+                        $j=0;
+                        if ($i%2==1)
+                                $j=$p[$i];
+                        //swap
+                        $tmp=$set[$j];
+                        $set[$j]=$set[$i];
+                        $set[$i]=$tmp;
+                        $i=1;
+                        $solutions[]=$set;
+                   }
+                   elseif ($p[$i]==0){
+                        $p[$i]=$i;
+                        $i++;
+                   }
+                }
+                return $solutions;
+        }
 }
+
